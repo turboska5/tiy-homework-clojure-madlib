63047bfda9b466e2b51e484129808f58ab7192ff
turboska5 <andrewrnagel@gmail.com>
2016-08-08 11:42:22 -0400

    Version 1.1: revised defn to include println

--- src/clojure_madlib/core.clj
+++ src/clojure_madlib/core.clj
@@ -8,19 +8,19 @@
 (defn -main []
     ;; define arguments
     (def adjective "luscious")
-    (def things1 "laptops")
-    (def things2 "cats")
+    (def animal "goat")
+    (def things "cat whiskers")
     (def song-title "Bohemian Rhapsody")
     (def celebrity "Michael Bolton")
-    (def adjective2 "smooth")
-    (def verb "vomit")
+    (def adjective2 "shiny")
+    (def verb-past-tense "puked")
     (def city "Rugby, ND")
     (def food "watermelon")
     (def person "George Washington")
 
     ;; define method mad-lib
-    (defn mad-lib [adj-1 things-plural-1 song-title-1 celebrity-1 adj-2 verb-1 city-1 food-1 things-plural-2 person-1] (str "I just got back from a pizza party with " person-1 ". Can you believe we got to eat " adj-1 " pizza in " city-1 "?! Everyone got to choose their own toppings. I made " food-1 " and " things-plural-1 " pizza, which is my favorite! They even stuffed the crust with " things-plural-2 ". How " adj-2 "! If that wasn't good enough already, " celebrity-1 " was there singing " song-title-1 ". I was so inspired by the music, I had to get up out of my seat and " verb-1 ".\n"))
+    (defn mad-lib [adj-1 animal-1 song-title-1 celebrity-1 adj-2 verb-past-tense-1 city-1 food-1 things-1 person-1] (println(str "I just got back from a pizza party with " person-1 ". Can you believe we got to eat " adj-1 " pizza in " city-1 "?! Everyone got to choose their own toppings. I made " food-1 " and " animal-1 " pizza, which is my favorite! They even stuffed the crust with " things-1 ". How " adj-2 "! If that wasn't good enough already, " celebrity-1 " was there singing " song-title-1 ". I was so inspired by the music, I got up out of my seat and " verb-past-tense-1 ".\n")))
 
     ;; call mad-lib with argument bindings
-    (println (mad-lib adjective, things1, song-title, celebrity, adjective2, verb, city, food, things2, person))
+    (mad-lib adjective, animal, song-title, celebrity, adjective2, verb-past-tense, city, food, things, person)
 )
